8d2eff82f062ed47f8467e61e81776525483cb5b
DavidJGooch <143031049+DavidJGooch@users.noreply.github.com>
2024-02-22 17:35:20 -0600

    fixing challenge to meet guidelines

--- PyBank/Bankmain.py
+++ PyBank/Bankmain.py
@@ -3,27 +3,47 @@ import csv
 
 #Set path for csv file
 csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
- 
-#initialize variables
-monthcount = 0
-total_profit = 0
-profit_loss = []
-#Open and read csv file
-with open(csvpath) as csv_file:
-    Bankcsv = csv.reader(csv_file, delimiter=",")
-    #Skip header row
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
-   #Count row number 
-    for row in Bankcsv:
-        monthcount += 1
-#print total months
-print("Total Months:", monthcount)
 
-#Total Profit/Loss:
-with open(csvpath) as csv_file:
-    Bankcsv = csv.reader(csv_file, delimiter=",")
-    #Skip header row
-    csv_header = next(csv_file)
-    total = sum(int(r[1]) for r in csv.reader(csv_file))
-print("Total:", total)
+#Variables
+monthtotal = 0
+nettotal = 0 
+grtincrease = 0
+grtdecrease = 0 
+grtmonthinc = ""
+grtmonthdec = ""
+prevmonth = 0
+change = 0
+
+with open(csvpath) as csvfile:
+    csvread = csv.reader(csvfile, delimiter=",")
+    csvhead = next(csvread)
+    for row in csvread:
+        if prevmonth != 0:
+            netchange = (prevmonth - int(row[1])) * -1
+            change += netchange
+            if netchange > grtincrease:
+                grtincrease = netchange
+                grtmonthinc = row[0]
+            if netchange < grtdecrease:
+                grtdecrease = netchange
+                grtmonthdec = row[0]
+        prevmonth = int(row[1])
+        monthtotal +=1
+        nettotal += int(row[1])
+
+analysis = os.path.join("PyBank","BankAnalysis","BankResults.txt")
+
+with open(analysis, 'w') as textfile:
+    textfile.write("Financial Analysis")
+    textfile.write('\n')
+    textfile.write("-----------------------------------")
+    textfile.write('\n')
+    textfile.write(f'Total Months: {monthtotal}')
+    textfile.write('\n')
+    textfile.write(f'Total: ${nettotal}')
+    textfile.write('\n')
+    textfile.write(f'Average Change: ${round(change/(monthtotal - 1),2)}')
+    textfile.write('\n')
+    textfile.write(f'Greatest Increase in Profits: {grtmonthinc} (${grtincrease})')
+    textfile.write('\n')
+    textfile.write(f'Greatest Decrease in Profits: {grtmonthdec} (${grtdecrease})')
--- PyPoll/Pollmain.py
+++ PyPoll/Pollmain.py
@@ -4,19 +4,42 @@ import csv
 #Set path for csv file
 csvpath = os.path.join("PyPoll", "Resources", "election_data.csv")
  
-#Total Months:
-#initialize month count
 votecount = 0
-#Open and read csv file
-with open(csvpath) as csv_file:
-    Pollcsv = csv.reader(csv_file, delimiter=",")
-    #Skip header row
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
-   #Count row number 
-    for row in Pollcsv:
-        votecount += 1
-#print total months
-print("Total Votes:", votecount)
+candidates = {}
 
+with open(csvpath) as csvfile:
+    csvread = csv.reader(csvfile, delimiter=",")
+    csvhead = next(csvread)
+    for row in csvread:
+        candidate = str(row[2])
+        if candidate not in candidates:
+            candidates[candidate]=1
+        else:
+            candidates[candidate]+=1
+        votecount+=1
 
+analysis = os.path.join("PyPoll","PollAnalysis","PollResults.txt")
+with open(analysis,'w') as textfile:
+    textfile.write("Election Results")
+    textfile.write("\n")
+    textfile.write("------------------------------")
+    textfile.write("\n")
+    textfile.write(f"Total Votes: {votecount}")
+    textfile.write("\n")
+    textfile.write("------------------------------")
+    textfile.write("\n")
+    for name,value in candidates.items():
+        textfile.write(f"{name}: {(value/votecount):.3%} ({value})")
+        textfile.write("\n")
+    textfile.write("------------------------------")
+    textfile.write("\n")
+    finaltally = 0
+    finalwinner = ""
+    for name,value in candidates.items():
+        if value > finaltally:
+            finaltally = value
+            del finalwinner
+            finalwinner = name
+    textfile.write(f"Winner: {finalwinner}")
+    textfile.write("\n")
+    textfile.write("------------------------------")
